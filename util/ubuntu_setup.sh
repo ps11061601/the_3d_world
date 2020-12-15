afc9cd86fd8b34f19cee3ef7fef85deeb4490544
Pooja Singh <49348346+ps11061601@users.noreply.github.com>
2020-12-14 20:38:21 -0500

    Update ubuntu_setup.sh

--- util/ubuntu_setup.sh
+++ util/ubuntu_setup.sh
@@ -3,65 +3,63 @@
 
 # Install apt-getable dependencies
 apt-get update \
-&& apt-get install -y \
-    build-essential \
-    cmake \
-    git \
-    libatlas-base-dev \
-    libboost-python-dev \
-    libeigen3-dev \
-    libgoogle-glog-dev \
-    libopencv-dev \
-    libsuitesparse-dev \
-    python-dev \
-    python-numpy \
-    python-opencv \
-    python-pip3 \
-    python-pyexiv2 \
-    python-pyproj \
-    python-scipy \
-    python-yaml \
-    wget \
+    && apt-get install -y \
+        build-essential \
+        cmake \
+        git \
+        libatlas-base-dev \
+        libeigen3-dev \
+        libgoogle-glog-dev \
+        libopencv-dev \
+        libsuitesparse-dev \
+        python3-dev \
+        python3-numpy \
+        python3-opencv \
+        python3-pip \
+        python3-pyproj \
+        python3-scipy \
+        python3-yaml \
+        curl \
+        vim \
 && apt-get clean \
 && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 
 # Install Ceres from source
 mkdir -p /source && cd /source && \
-wget http://ceres-solver.org/ceres-solver-1.14.0.tar.gz && \
-tar xvzf ceres-solver-1.14.0.tar.gz && \
-cd /source/ceres-solver-1.14.0 && \
-mkdir -p build && cd build && \
-cmake .. -DCMAKE_C_FLAGS=-fPIC -DCMAKE_CXX_FLAGS=-fPIC -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF && \
-make install && \
-cd / && \
-rm -rf /source/ceres-solver-1.14.0 && \
-rm -f /source/ceres-solver-1.14.0.tar.gz
+    curl -L http://ceres-solver.org/ceres-solver-1.14.0.tar.gz | tar xz && \
+    cd /source/ceres-solver-1.14.0 && \
+    mkdir -p build && cd build && \
+    cmake .. -DCMAKE_C_FLAGS=-fPIC -DCMAKE_CXX_FLAGS=-fPIC -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF && \
+    make -j4 install && \
+    cd / && rm -rf /source/ceres-solver-1.14.0
 
 
 # Install opengv from source
-rm -rf /source/opengv
 mkdir -p /source && cd /source && \
-git clone https://github.com/paulinus/opengv.git && \
-cd /source/opengv && \
-git submodule update --init --recursive && \
-mkdir -p build && cd build && \
-cmake .. -DBUILD_TESTS=OFF -DBUILD_PYTHON=ON -DPYBIND11_PYTHON_VERSION=2.7 && \
-make install && \
-cd / && \
-rm -rf /source/opengv
+    git clone https://github.com/paulinus/opengv.git && \
+    cd /source/opengv && \
+    git submodule update --init --recursive && \
+    mkdir -p build && cd build && \
+    cmake .. -DBUILD_TESTS=OFF \
+             -DBUILD_PYTHON=ON \
+             -DPYBIND11_PYTHON_VERSION=3.6 \
+             -DPYTHON_INSTALL_DIR=/usr/local/lib/python3.6/dist-packages/ \
+             && \
+    make install && \
+    cd / && rm -rf /source/opengv
 
 
 # Install python requirements
 pip3 install exifread==2.1.2 \
-            gpxpy==1.1.2 \
-            networkx==1.11 \
-            numpy \
-            pyproj==1.9.5.1 \
-            pytest==3.0.7 \
-            python-dateutil==2.6.0 \
-            PyYAML==3.12 \
-            scipy \
-            xmltodict==0.10.2 \
-            cloudpickle==0.4.0 \
-            loky==1.2.1
+                 gpxpy==1.1.2 \
+                 networkx==1.11 \
+                 numpy \
+                 pyproj==1.9.5.1 \
+                 pytest==3.0.7 \
+                 python-dateutil==2.6.0 \
+                 PyYAML==3.12 \
+                 scipy \
+                 xmltodict==0.10.2 \
+                 loky \
+                 repoze.lru
